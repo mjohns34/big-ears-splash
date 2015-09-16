2ad7c622112dcc588ff0c840319f33323f32f2ff
Miranda Johnson <miranda@insightdesigns.com>
2015-09-16 17:36:40 -0600

    Instantiate empty header & footer chunks

---
+++ core/cache/mgr/smarty/default/01a2a39f9cf1fdd8acc4bed8d02936e40f0d5711.file.create.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version Smarty-3.0.4, created on 2015-09-17 01:28:24
+         compiled from "/Applications/MAMP/htdocs/big-ears-splash/manager/templates/default/element/chunk/create.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:89780118255f9fb18bae3d9-20103033%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '01a2a39f9cf1fdd8acc4bed8d02936e40f0d5711' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/big-ears-splash/manager/templates/default/element/chunk/create.tpl',
+      1 => 1439958290,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '89780118255f9fb18bae3d9-20103033',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<div id="modx-panel-chunk-div"></div>
+<?php echo $_smarty_tpl->getVariable('onChunkFormPrerender')->value;?>
---
+++ core/cache/mgr/smarty/default/7432616444dd2807ca4c7587939d3be13a02c741.file.update.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version Smarty-3.0.4, created on 2015-09-17 01:33:55
+         compiled from "/Applications/MAMP/htdocs/big-ears-splash/manager/templates/default/element/chunk/update.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:162503777555f9fc63667380-82879124%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '7432616444dd2807ca4c7587939d3be13a02c741' => 
+    array (
+      0 => '/Applications/MAMP/htdocs/big-ears-splash/manager/templates/default/element/chunk/update.tpl',
+      1 => 1439958290,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '162503777555f9fc63667380-82879124',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<div id="modx-panel-chunk-div"></div>
+<?php echo $_smarty_tpl->getVariable('onChunkFormPrerender')->value;?>
