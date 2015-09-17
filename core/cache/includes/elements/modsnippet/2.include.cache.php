94de30d357983d945c3c3d6c76ef3108dab50f50
Miranda Johnson <miranda@insightdesigns.com>
2015-09-16 22:32:42 -0600

    New snippet for parsing XML

---
+++ core/cache/includes/elements/modsnippet/2.include.cache.php
@@ -0,0 +1,16 @@
+<?php
+$file = file_get_contents($url);
+
+if ($xml = simplexml_load_string($file)) {
+	$items = $xml->xpath("//item");
+	
+	foreach ($items as $i => $item) {
+		if ($i >= 3) { break; }
+		$output .= print_r($item, 1);
+	}
+	
+} else {
+	$output = 'false';
+}
+return $output;
+return;
