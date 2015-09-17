dfac5e4cff52dbf1f7c803d5c571d36f1e89a09a
Miranda Johnson <miranda@insightdesigns.com>
2015-09-16 23:04:30 -0600

    Format XML data as HTML

--- assets/snippets/getXmlFeed.php
+++ assets/snippets/getXmlFeed.php
@@ -1,15 +1,19 @@
 <?php
 $file = file_get_contents($url);
 
-if ($xml = simplexml_load_string($file)) {
+if ($xml = simplexml_load_string($file, null, LIBXML_NOCDATA)) {
 	$items = $xml->xpath("//item");
 	
 	foreach ($items as $i => $item) {
-		if ($i >= 3) { break; }
-		$output .= print_r($item, 1);
+		if ($i >= $num) { break; }
+		if (strlen($item->description) > 80) {
+			$description = '<p>' . trim(preg_replace('/^(.{' . 80 . ',}? ).*$/is', '$1', strip_tags($item->description))) . '...</p>';
+		} else {
+			$description = $item->description;
+		}
+		
+		$output .= '<h3><a href="' . $item->link . '" target="_blank"><h3>' . $item->title . '</a></h3>' . $description;
 	}
-	
-} else {
-	$output = 'false';
-}
+    
+} 
 return $output;
